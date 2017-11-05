e62c248327711b1013c36756dd359b15c9629002
1115342730 <1115342730@qq.com>
2017-11-05 14:24:32 +0800

    update

---
+++ e19-scrapy_project/shiyanlou/shiyanlou/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ShiyanlouItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+class CourseItem(scrapy.Item):
+	name = scrapy.Field()
+	descripition = scrapy.Field()
+	type = scrapy.Field()
+	students = scrapy.Field()
---
+++ e19-scrapy_project/shiyanlou/shiyanlou/spiders/courses.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from shiyanlou.items import CourseItem
+
+class CoursesSpider(scrapy.Spider):
+	name = 'courses'
+
+	@property 
+	def start_urls(self):
+		url_tmp1 = 'https://www.shiyanlou.com/course/?category=all&course_type=all&fee=all&tag=all&page={}'
+		return (url_tmp1.format(i) for i in range(1,23))
+
+	def parse(self,response):
+		for course in response.css('div.course-body'):
+			item = CouresItem({
+
+			'name': course.css('div.course-name::text').extract_first(),
+			'descripition': course.css('div.course-desc::text').extract_first(),
+			'type': course.css('div.course-footer span.pull-right::text').extract_first(default = 'free'),
+			'students': course.xpath('.//span[contains(@class,"pull-left")]/text()[2]').re_first('[^\d]*(\d*)[^\d]*')
+			
+			})
+			yield item
+
